f6678b840194ebcf205830b8ed284daab47122b4
wauwau0977 <patrick.heusser@gmail.com>
2025-03-18 06:17:50 +0000

    dev

--- gemma3_inference_ollama.py
+++ gemma3_inference_ollama.py
@@ -1,7 +1,7 @@
 import ollama
 
 class GemmaInferenceOllama:
-    def __init__(self, model_name="gemma3:27b", max_seq_length=4096):
+    def __init__(self, model_name="gemma3:27b", max_seq_length=8000):
         self.model_name = model_name
         self.max_seq_length = max_seq_length
 
